289da4a466c8b43b3e188a7125eca315da56d454
Plastic Digits <plasticdigits@protonmail.com>
2023-07-10 20:30:29 -0700

    Add emergency owner and pausable methods to entitystoreerc20

--- contracts/EntityStoreERC20.sol
+++ contracts/EntityStoreERC20.sol
@@ -5,13 +5,15 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "./interfaces/ILocation.sol";
 import "./interfaces/ILocationController.sol";
 
 //Permisionless EntityStoreERC20
 //Deposit/withdraw/transfer tokens that are stored to a particular entity
 //deposit/withdraw/transfers are restricted to the entity's current location.
-contract EntityStoreERC20 {
+contract EntityStoreERC20 is Ownable, Pausable {
     using EnumerableSet for EnumerableSet.UintSet;
     using SafeERC20 for IERC20;
 
@@ -35,7 +37,7 @@ contract EntityStoreERC20 {
         _;
     }
 
-    constructor(ILocationController _locationController) {
+    constructor(ILocationController _locationController) Ownable() {
         locationController = _locationController;
     }
 
@@ -44,7 +46,7 @@ contract EntityStoreERC20 {
         uint256 _entityId,
         IERC20 _token,
         uint256 _wad
-    ) external onlyEntitysLocation(_entity, _entityId) {
+    ) external onlyEntitysLocation(_entity, _entityId) whenNotPaused {
         uint256 expectedShares = convertTokensToShares(_token, _wad);
         uint256 initialTokens = _token.balanceOf(address(this));
         _token.safeTransferFrom(
@@ -64,7 +66,7 @@ contract EntityStoreERC20 {
         uint256 _entityId,
         IERC20 _token,
         uint256 _wad
-    ) external onlyEntitysLocation(_entity, _entityId) {
+    ) external onlyEntitysLocation(_entity, _entityId) whenNotPaused {
         uint256 shares = convertTokensToShares(_token, _wad);
         entityStoredERC20Shares[_entity][_entityId][_token] -= shares;
         totalShares[_token] -= shares;
@@ -85,6 +87,7 @@ contract EntityStoreERC20 {
         external
         onlyEntitysLocation(_fromEntity, _fromEntityId)
         onlyEntitysLocation(_toEntity, _toEntityId)
+        whenNotPaused
     {
         uint256 shares = convertTokensToShares(_token, _wad);
         entityStoredERC20Shares[_fromEntity][_fromEntityId][_token] -= shares;
@@ -96,7 +99,7 @@ contract EntityStoreERC20 {
         uint256 _entityId,
         ERC20Burnable _token,
         uint256 _wad
-    ) external onlyEntitysLocation(_entity, _entityId) {
+    ) external onlyEntitysLocation(_entity, _entityId) whenNotPaused {
         uint256 shares = convertTokensToShares(_token, _wad);
         entityStoredERC20Shares[_entity][_entityId][_token] -= shares;
         totalShares[_token] -= shares;
@@ -124,5 +127,13 @@ contract EntityStoreERC20 {
     function getSharesPerToken(IERC20 _token) external view returns (uint256) {
         if (totalShares[_token] == 0) return SHARES_PRECISION;
         return totalShares[_token] / _token.balanceOf(address(this));
+    }
+
+    //Escape hatch for emergency use
+    function recoverERC20(address tokenAddress) external onlyOwner {
+        IERC20(tokenAddress).safeTransfer(
+            _msgSender(),
+            IERC20(tokenAddress).balanceOf(address(this))
+        );
     }
 }
